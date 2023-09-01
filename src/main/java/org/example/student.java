fc3ce03a9a15601eeac2cccd686bd5ad2fa01180
max <22000152@handong.ac.kr>
2023-09-01 16:53:13 +0900

    first commit

---
+++ src/main/java/org/example/student.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class student {
+    private String first;
+    private static String second;
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+
+    public student(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "student"+first+" "+second;
+    }
+}
