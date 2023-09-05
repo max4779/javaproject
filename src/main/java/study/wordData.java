58f071b6c8ef8a564ee1db00c83b0b012329a5a6
max <22000152@handong.ac.kr>
2023-09-06 07:08:35 +0900

    english word study tool second

---
+++ src/main/java/study/wordData.java
@@ -0,0 +1,52 @@
+package study;
+
+public class wordData {
+
+    private int id;
+    private int level;
+    private String word;
+    private String meaning;
+
+
+    wordData(){
+    }
+
+    wordData(int id, int level, String word, String meaning){
+        this.id=id;
+        this.level=level;
+        this.word=word;
+        this.meaning=meaning;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
